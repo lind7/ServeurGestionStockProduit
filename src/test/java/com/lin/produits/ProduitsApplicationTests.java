87ed8cf6a1ae5fd0926c397bde4ca7cf52558f0c
lind7 <kakeulinda7@gmail.com>
2020-02-05 03:08:40 +0100

    Initial commit

---
+++ src/test/java/com/lin/produits/ProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
